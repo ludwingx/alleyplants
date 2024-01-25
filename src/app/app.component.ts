87e4b4935c963c1ce6174ba8f3d73d3dc317a430
ludwingx <ludwingytx@gmail.com>
2024-01-25 14:39:16 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { AnnouncementComponent } from './components/announcement/announcement.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, HomeComponent, ProductsComponent, AnnouncementComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
